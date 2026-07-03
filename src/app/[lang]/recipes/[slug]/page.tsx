68f29bccd28b7973bec20c2051459cea9c4c1a90
mchamow <michal.chamow@sabre.com>
2026-07-03 10:38:00 +0200

    fix: replace hardcoded posts links with internal routing
    Make sure language is correct after redirection

--- src/app/[lang]/recipes/[slug]/page.tsx
+++ src/app/[lang]/recipes/[slug]/page.tsx
@@ -78,7 +78,12 @@ export default async function RecipeDetailPage({
       {/* Content */}
       <div
         className="prose"
-        dangerouslySetInnerHTML={{ __html: post.content }}
+        dangerouslySetInnerHTML={{
+          __html: post.content.replace(
+            /\/pl\/recipes\/([a-zA-Z0-9_-]+)/g,
+            `/${lang}/recipes/$1`
+          ),
+        }}
       />
 
       {/* Back */}
