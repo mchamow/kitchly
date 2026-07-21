6991fe562c55a7da9a277359d473193a74d706f4
mchamow <michal.chamow@sabre.com>
2026-07-21 21:06:48 +0200

    refactor: remove categories

--- src/app/[lang]/categories/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default async function CategoryDetailPage({
-  params,
-}: PageProps<"/[lang]/categories/[slug]">) {
-  const { lang } = await params;
-  redirect(`/${lang}/recipes`);
-}
--- src/app/[lang]/categories/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default async function CategoriesPage({
-  params,
-}: PageProps<"/[lang]/categories">) {
-  const { lang } = await params;
-  redirect(`/${lang}/recipes`);
-}
